bb5160b3ac748b74f49a04b418481f930faa46aa
avacosta <avacosta@ossesanjuan.com.ar>
2023-10-18 09:54:51 -0300

    fin parte 2

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -2,19 +2,34 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-var factory = new ConnectionFactory(){HostName = "localhost"};
-using var connection = factory.CreateConnection();
-using (var channel = connection.CreateModel())
+class Program
 {
-    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-    var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (model, ea) =>
+    static void Main(string[] args)
     {
-        var body = ea.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
-        Console.WriteLine($" [x] Received {message}" );
-    };
-    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
-    Console.WriteLine(" Press [enter] to exit.");
-    Console.ReadLine();
-};
+        var factory = new ConnectionFactory(){HostName = "localhost"};
+        using var connection = factory.CreateConnection();
+        using (var channel = connection.CreateModel())
+        {
+            //channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            //cola con nombre aleatorio
+            var queueName = channel.QueueDeclare().QueueName;
+            //asociamos la cola a la exchange
+            channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+            Console.WriteLine(" [*] Waiting for logs.");
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}" );
+            };
+            //channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        };
+}
+}
+
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,18 +1,35 @@
 ﻿using System.Text;
 using RabbitMQ.Client;
 
-var factory = new ConnectionFactory(){HostName = "localhost"};
-using (var connection = factory.CreateConnection())
-{   
-    using (var channel = connection.CreateModel())
+class Program
+{
+    static void Main(string[] args)
     {
-        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-        string message = "Hello World!";
-        var body = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
-        Console.WriteLine($" [x] Sent {message}");
-    }
-    Console.WriteLine(" Press [enter] to exit.");
-    Console.ReadLine();
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                //string message = "Hello World!";
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+                string message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                //channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+                Console.WriteLine($" [x] Sent {message}");
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
 
+        }
+    }
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    }
 }
+
+
+
+
